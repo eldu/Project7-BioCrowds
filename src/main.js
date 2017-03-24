8d76bfc8aade90810192c6035f69caa61d5bc6c5
Ellen Duong <ellen@huntsman-ve565-1532.apn.wlan.upenn.edu>
2017-03-24 16:56:13 -0400

    Basic scene

--- src/main.js
+++ src/main.js
@@ -13,25 +13,50 @@ function onLoad(framework) {
   var gui = framework.gui;
   var stats = framework.stats;
 
+  //renderer.setClearColor (0xbac2d1, 1);
+
+  // initialize a simple box and material
+  var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+  directionalLight.color.setHSL(0.1, 1, 0.95);
+  directionalLight.position.set(1, 3, 2);
+  directionalLight.position.multiplyScalar(10);
+  scene.add(directionalLight);
+
   // set camera position
-  camera.position.set(1, 1, 200);
-  camera.lookAt(new THREE.Vector3(0,0,0));
+  camera.position.set(1, 50, 200);
+  camera.lookAt(new THREE.Vector3(-1, -1, -1));
 
 
-  // Plane
+  // Voronoi Diagram Plane
   var planeGeo = new THREE.PlaneGeometry(1000, 1000);
-  var planeMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide});
+  // var planeMaterial = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide});
+  var planeMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      u_amount: {
+        type: "f",
+        value: 1.0
+      },
+      u_albedo: {
+        type: 'v3',
+        value: new THREE.Color('#ffffff')
+      }
+    },
+    vertexShader: require('./shaders/voronoi-vert.glsl'),
+    fragmentShader: require('./shaders/voronoi-frag.glsl')
+  });
   var planeMesh = new THREE.Mesh(planeGeo, planeMaterial);
-  planeMesh.rotateX(90);
+  planeMesh.rotateX(-Math.PI / 2.0);
   scene.add(planeMesh);
 
   // TEST OBJECTS DELETE ONCE DONE
   // Origin
-  var geometry = new THREE.SphereGeometry( 5, 32, 32 );
-  var material = new THREE.MeshBasicMaterial( {color: 0xff0000} );
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshLambertMaterial( {color: 0xff0000} );
   var sphere = new THREE.Mesh( geometry, material );
-  sphere.position.set(0, 0, 0);
+  sphere.position.set(0, 0.5, 0);
   scene.add( sphere );
+
+  // scene.add(new THREE.AmbientLight(0xFFFFFF));
 
   // edit params and listen to changes like this
   // more information here: https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
---
+++ src/shaders/voronoi-frag.glsl
@@ -0,0 +1,11 @@
+
+varying vec2 f_uv;
+varying vec3 f_normal;
+varying vec3 f_position;
+
+uniform float u_amount;
+uniform vec3 u_albedo;
+
+void main() {
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
